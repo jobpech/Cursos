f96b1653582fc3c27b8f4e34d2dcfc8fa83fe599
Job <158128362+jobpech@users.noreply.github.com>
2025-04-08 12:05:19 -0600

    Se creo archivo

---
+++ curso.py
@@ -0,0 +1,12 @@
+#Curso de Python para data science
+
+##Instalar e importar Bibliotecas
+
+`pip install matplotlib`
+
+##Importar bibliotecas
+`import matplotlib`
+
+#Verificar version
+`matplotlib.__version__`
+
